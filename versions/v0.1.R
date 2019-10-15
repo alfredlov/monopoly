35d31f18f4960d75307d1823022bc2179cc93cf0
Alfred Løvgren <alfredlovgr1@gmail.com>
2019-10-15 12:55:04 +0200

    pseudo

--- versions/v0.1.R
+++ versions/v0.1.R
@@ -1,1 +1,19 @@
 # Monopoly simulation
+
+#import gameboard as dataset
+
+
+n <- 2 #spillere
+
+
+
+# loop game
+  #init game 
+    #definere data.frame for spillere
+      #slik at alt resetes hver gang
+      #definere én strategi pr spiller
+      #velg hvem som starter (variabel i dataframe som har verdi)
+  #start game, for løkke som går så lenge begge spillere har penger > 0
+    #kast terning
+    #kjør håndter ny posisjon 
+
